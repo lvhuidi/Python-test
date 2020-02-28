0e969c726ff29ad6b6365d788ff325c623958856
lvhuidi <2235442390@qq.com>
2020-02-28 09:47:42 +0800

    计算输入数值是否大于0[C

---
+++ number-test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# test num range and print
+num = int(input("please enter number: "))
+if num < 0:
+    print("less lan zero")
+elif num == 0:
+    print("this is zero")
+else:
+    print("more than zero")
